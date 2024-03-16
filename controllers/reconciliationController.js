df12c8a2fd811f1b59f5bb38205e261787b005d2
narendra Patidar <nk580585@gmail.com>
2024-03-16 16:25:55 +0530

    recon

--- controllers/reconciliationController.js
+++ controllers/reconciliationController.js
@@ -25,7 +25,7 @@ const createReconciliation = async (req, res) => {
         for (const item of aggregatedData) {
             let status = "ER";
             if (item.purchaserBillData.length > 0) {
-             
+
                 for (const itemInItem of item.purchaserBillData) {
                     status = "ER";
                     if (itemInItem.invoiceNo === item.invoiceNo &&
